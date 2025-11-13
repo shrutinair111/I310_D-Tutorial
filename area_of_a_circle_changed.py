5dba192d133afc9171007b63147dddcbe45ef603
Shruti Nair <shrutinair@Shrutis-MacBook-Air.local>
2025-11-13 01:22:57 -0600

    Add updated circle tutorial file

--- area_of_a_circle_changed.py
+++ area_of_a_circle_changed.py
@@ -4,6 +4,7 @@ def compute_area_of_circle(radius):
 	return area
 
 radius1 = 30
+radius1 = 40000
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
