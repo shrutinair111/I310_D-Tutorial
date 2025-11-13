3376dc36c8ebcbc9d3508ad11cf71c4e7e28e93f
shrutinair111 <shruti.nair@utexas.edu>
2025-11-13 00:01:22 -0600

    Add files via upload

---
+++ area_of_a_circle_changed.py
@@ -0,0 +1,12 @@
+def compute_area_of_circle(radius):
+	pi = 3.14
+	area = pi * radius * radius
+	return area
+
+radius1 = 30
+area1 = compute_area_of_circle(radius1)
+print(f"The area of circle with radius {radius1} is: {area1}")
+
+radius2 = 35
+area2 = compute_area_of_circle(radius1)
+print(f"The area of circle with radius {radfius2} is: {area2}")
